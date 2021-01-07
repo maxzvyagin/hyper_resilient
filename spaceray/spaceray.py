7f570c92f18edb4e2ba1bec0c5237fe364f632e8
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2021-01-07 14:31:57 -0800

    Update spaceray.py

--- spaceray/spaceray.py
+++ spaceray/spaceray.py
@@ -39,45 +39,45 @@ def run_experiment(args, func, mode="max", metric="average_res",
         except:
             print("Ray.init failed twice.")
         print("WARNING: could not connect to existing Ray Cluster. Ignore warning if only running on single node.")
-    # ray.init()
-    space, bounds = get_trials(args)
-    # Run and aggregate the results
-    results = []
-    sk_optimizer_results = []
-    i = 0
-    error_name = args.out.split(".csv")[0]
-    error_name += "_error.txt"
-    error_file = open(error_name, "w")
-    for section in tqdm(space):
-        optimizer = Optimizer(section, random_state=0)
-        search_algo = SkOptSearch(optimizer, list(bounds.keys()), metric=metric, mode=mode)
-        try:
-            analysis = tune.run(func, search_alg=search_algo, num_samples=int(args.trials),
-                                resources_per_trial={'cpu': cpu, 'gpu': gpu},
-                                local_dir=ray_dir)
-            results.append(analysis)
-            #sk_optimizer_results.append(optimizer.get_result())
-            sk_optimizer_results.append(search_algo.optimizer_results)
-        except Exception as e:
-            error_file.write("Unable to complete trials in space " + str(i) + "... Exception below.")
-            error_file.write(str(e))
-            error_file.write("\n\n")
-            print("Unable to complete trials in space " + str(i) + "... Continuing with other trials.")
-        i += 1
-
-    print("Measured time needed to run trials: ")
-    execution_time = (time.time() - start_time)
-    print('Execution time in seconds: ' + str(execution_time))
-
-    error_file.close()
-
-    # save results to specified csv file
-    all_pt_results = results[0].results_df
-    for i in range(1, len(results)):
-        all_pt_results = all_pt_results.append(results[i].results_df)
-
-    all_pt_results.to_csv(args.out)
-    print("Ray Tune results have been saved at " + args.out + " .")
-    print("Error file has been saved at " + error_name + " .")
-
-    return sk_optimizer_results
+    print(ray.cluster_resources())
+    # space, bounds = get_trials(args)
+    # # Run and aggregate the results
+    # results = []
+    # sk_optimizer_results = []
+    # i = 0
+    # error_name = args.out.split(".csv")[0]
+    # error_name += "_error.txt"
+    # error_file = open(error_name, "w")
+    # for section in tqdm(space):
+    #     optimizer = Optimizer(section, random_state=0)
+    #     search_algo = SkOptSearch(optimizer, list(bounds.keys()), metric=metric, mode=mode)
+    #     try:
+    #         analysis = tune.run(func, search_alg=search_algo, num_samples=int(args.trials),
+    #                             resources_per_trial={'cpu': cpu, 'gpu': gpu},
+    #                             local_dir=ray_dir)
+    #         results.append(analysis)
+    #         #sk_optimizer_results.append(optimizer.get_result())
+    #         sk_optimizer_results.append(search_algo.optimizer_results)
+    #     except Exception as e:
+    #         error_file.write("Unable to complete trials in space " + str(i) + "... Exception below.")
+    #         error_file.write(str(e))
+    #         error_file.write("\n\n")
+    #         print("Unable to complete trials in space " + str(i) + "... Continuing with other trials.")
+    #     i += 1
+    #
+    # print("Measured time needed to run trials: ")
+    # execution_time = (time.time() - start_time)
+    # print('Execution time in seconds: ' + str(execution_time))
+    #
+    # error_file.close()
+    #
+    # # save results to specified csv file
+    # all_pt_results = results[0].results_df
+    # for i in range(1, len(results)):
+    #     all_pt_results = all_pt_results.append(results[i].results_df)
+    #
+    # all_pt_results.to_csv(args.out)
+    # print("Ray Tune results have been saved at " + args.out + " .")
+    # print("Error file has been saved at " + error_name + " .")
+    #
+    # return sk_optimizer_results
