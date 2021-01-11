1c2c3e8b56d2bb9ee61a3b2af22f68f68b62ebde
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2021-01-11 11:17:43 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     name='spaceray',
     license="MIT",
     description='Integration of HyperSpace with Ray Tune search automation.',
-    version='v0.0.11',
+    version='v0.0.12',
     long_description=README,
     url='https://github.com/maxzvyagin/spaceray',
     packages=setuptools.find_packages(),
