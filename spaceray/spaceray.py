00d88caab8ffc19a00bda94fd9fae19200d47396
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2021-02-18 10:18:48 -0800

    Update spaceray.py

--- spaceray/spaceray.py
+++ spaceray/spaceray.py
@@ -61,7 +61,8 @@ def run_specific_spaces(spaces, bounds, intermediate_dir, func, trials, mode, me
             opt_result = optimizer.get_result()
             f = open(intermediate_dir + "/optimizer_result" + str(i) + ".pkl", "wb+")
             pickle.dump(opt_result, f)
-        except:
+        except Exception as e:
+            print(e)
             print("Failure to run space {}, contintuing with next spaces.".format(i))
     return 0
 
